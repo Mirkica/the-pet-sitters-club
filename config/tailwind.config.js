8ed724b15e9f64d13a50559018a68f717efa9d7a
Mirela Mustic <mirela.mustic00@gmail.com>
2024-08-22 00:36:00 +0100

    create shared navbar

--- config/tailwind.config.js
+++ config/tailwind.config.js
@@ -1,22 +1,27 @@
-const defaultTheme = require('tailwindcss/defaultTheme')
+import { fontFamily as _fontFamily } from "tailwindcss/defaultTheme";
 
-module.exports = {
-  content: [
-    './public/*.html',
-    './app/helpers/**/*.rb',
-    './app/javascript/**/*.js',
-    './app/views/**/*.{erb,haml,html,slim}'
-  ],
-  theme: {
-    extend: {
-      fontFamily: {
-        sans: ['Inter var', ...defaultTheme.fontFamily.sans],
-      },
+export const content = [
+  "./public/*.html",
+  "./app/helpers/**/*.rb",
+  "./app/javascript/**/*.js",
+  "./app/views/**/*.{erb,haml,html,slim}",
+];
+export const theme = {
+  extend: {
+    colors: {
+      // Custom color theme
+      "custom-green": "#bae681",
+      "custom-red": "#d29293",
+      "custom-pink": "#f46cb1",
+      "custom-dark-pink": "#f047a7",
+    },
+    fontFamily: {
+      sans: ["Inter var", ..._fontFamily.sans],
     },
   },
-  plugins: [
-    require('@tailwindcss/forms'),
-    require('@tailwindcss/typography'),
-    require('@tailwindcss/container-queries'),
-  ]
-}
+};
+export const plugins = [
+  require("@tailwindcss/forms"),
+  require("@tailwindcss/typography"),
+  require("@tailwindcss/container-queries"),
+];
--- db/schema.rb
+++ db/schema.rb
@@ -20,7 +20,7 @@ ActiveRecord::Schema[7.2].define(version: 2024_08_21_152450) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "name"
-    t.index ["email"], name: "index_users_on_email", unique: true
-    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+    t.index [ "email" ], name: "index_users_on_email", unique: true
+    t.index [ "reset_password_token" ], name: "index_users_on_reset_password_token", unique: true
   end
 end
