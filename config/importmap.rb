f89991df922ba8eeaf7e62bd91672baefc383504
Mirela Mustic <mirela.mustic00@gmail.com>
2024-08-22 15:15:50 +0100

    fix issue with sign_out path

--- config/importmap.rb
+++ config/importmap.rb
@@ -1,9 +1,7 @@
 # Pin npm packages by running ./bin/importmap
 
+pin "tailwindcss", to: "https://cdn.tailwindcss.com", preload: true
 pin "application", preload: true
-pin "tailwindcss", to: "https://cdn.tailwindcss.com"
-pin "application"
-pin "@hotwired/turbo-rails", to: "turbo.min.js"
-pin "@hotwired/stimulus", to: "stimulus.min.js"
-pin "@hotwired/stimulus-loading", to: "stimulus-loading.js"
+pin "@hotwired/stimulus", to: "stimulus.min.js", preload: true
+pin "@hotwired/turbo-rails", to: "turbo.min.js", preload: true
 pin_all_from "app/javascript/controllers", under: "controllers"
