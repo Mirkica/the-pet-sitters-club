3b38a5e44e3c6cf7f7ea34db3eef4cc42078220c
Mirela Mustic <mirela.mustic00@gmail.com>
2024-08-22 01:49:56 +0100

    solve linting issues

--- config/importmap.rb
+++ config/importmap.rb
@@ -1,7 +1,7 @@
 # Pin npm packages by running ./bin/importmap
 
-pin 'application', preload: true
-pin 'tailwindcss', to: 'https://cdn.tailwindcss.com'
+pin "application", preload: true
+pin "tailwindcss", to: "https://cdn.tailwindcss.com"
 pin "application"
 pin "@hotwired/turbo-rails", to: "turbo.min.js"
 pin "@hotwired/stimulus", to: "stimulus.min.js"
